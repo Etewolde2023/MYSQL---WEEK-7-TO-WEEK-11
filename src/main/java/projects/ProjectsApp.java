19dc3fd159bce503f7c90a25e0ffd634c702d34d
Etewolde2023 <ermitewolde@gmail.com>
2023-07-11 19:56:48 -0400

    Codes for Week 7 and Week 8 assignments uploaded

---
+++ src/main/java/projects/ProjectsApp.java
@@ -0,0 +1,12 @@
+package projects;
+
+import projects.dao.DbConnection;
+
+public class ProjectsApp {
+
+	public static void main(String[] args) {
+		DbConnection.getConnection();
+
+	}
+
+}
---
+++ src/main/java/projects/exception/DbException.java
@@ -0,0 +1,27 @@
+package projects.exception;
+
+@SuppressWarnings("serial")
+public class DbException extends RuntimeException {
+
+	public DbException() {
+		
+	}
+
+	public DbException(String message) {
+		super(message);
+		
+	}
+
+	public DbException(Throwable cause) {
+		super(cause);
+		
+	}
+
+	public DbException(String message, Throwable cause) {
+		super(message, cause);
+		
+	}
+
+	}
+
+
